b87282e503e0675c26d57a6deb02868a58f1ba10
maximilien roussel <ro.maximilien@gmail.com>
2021-01-28 16:17:20 +0100

    nettoyage

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -16,6 +16,7 @@ class ArticleType extends AbstractType
             ->add('Nom')
             ->add('Price')
             ->add('Description');
+            //$builder->add('imageFile',Filetype) 
     }
 
     public function configureOptions(OptionsResolver $resolver)
