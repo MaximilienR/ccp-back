e0c9c634d0dc5df6aa02833642410416024da170
maximilien roussel <ro.maximilien@gmail.com>
2021-01-28 15:56:13 +0100

    nettoyage

--- src/Entity/Belts.php
+++ src/Entity/Belts.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\BeltsRepository;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Constraints\Image;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField;
 /**
  * @ORM\Entity(repositoryClass=BeltsRepository::class)
  * @Vich\Uploadable()
@@ -33,9 +36,43 @@ class Belts
      *
      */
     private $imageFile;
+    
     public function getId(): ?int
     {
         return $this->id;
     }
-    
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+
+    public function setImage(?string $image)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var null|DateTime
+     */
+    private $updated_at;
+
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+
+
+    public function setImageFile( ?File $imageFile ): void {
+        $this->imageFile = $imageFile;
+        if($this->imageFile instanceof UploadedFile){
+            $this->updated_at = new \DateTime('now');
+        }
+        //return $this;
+    }
 }
